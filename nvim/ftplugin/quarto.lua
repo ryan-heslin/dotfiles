1b89d530574be0fff1914a853d8fa8e3d47dd97a
ryan-heslin <rh3788@american.edu>
2022-08-12 13:29:06 -0400

    Add stylua.toml

--- nvim/ftplugin/quarto.lua
+++ nvim/ftplugin/quarto.lua
@@ -1,4 +1,5 @@
 require("abbrev")
+local opts = {silent = true, noremap = true, buffer = true}
 dofile(vim.api.nvim_get_runtime_file("ftplugin/r.lua", false)[1])
 dofile(vim.api.nvim_get_runtime_file("ftplugin/tex.lua", false)[1])
 dofile(vim.api.nvim_get_runtime_file("ftplugin/md.lua", false)[1])
@@ -9,4 +10,8 @@ vim.cmd(
         .. vim.fn.stdpath("data")
         .. "/plugged/Nvim-R/ftplugin/rmd_nvimr.vim"
 )
-vim.keymap.set({"n"}, "<leader>jj", ":lua send_chunk()<CR>", opts)
+vim.keymap.set({"n", "v"}, [[\jj]], ":lua run_chunk()<CR>", opts)
+vim.keymap.set({"n", "v"}, [[\rr]], ":lua run_all_chunks()<CR>", opts)
+vim.keymap.set({"n", "v"}, [[\l]], ":lua run_line()<CR>", opts)
+vim.keymap.set({"n", "v"}, [[\ss]], ":lua send_visual_selection()<CR>", opts)
+vim.keymap.set({"n", "v"}, [[\ca]], ":lua run_all_chunks(nil, vim.fn.line('.'))<CR>", opts)
--- nvim/lua/quarto.lua
+++ nvim/lua/quarto.lua
@@ -1,5 +1,20 @@
 -- Patterns to match chunk beginning and end lines
 --
+-- TODO use unpack to handle parser with multiple return values
+
+-- Form text into newline-separated string, adding
+-- space to ensure last line is evaluated
+collapse_text = function(text)
+    table.insert(text, " ")
+    return table.concat(text, "\n")
+end
+
+create_send_function = function(parser, sender)
+    return function(...)
+        local result = parser(...)
+        return sender(result)
+    end
+end
 chunk_start = "^%s*```{python.*$"
 chunk_end = "^%s*```$"
 
@@ -8,13 +23,14 @@ run_chunk = function(buffer)
     if buffer == nil then
         buffer = 0
     end
+    local buffer_name = vim.api.nvim_buf_get_name(buffer)
+
     -- 1-based index!
-    local current_line = vim.fn.line(".")
+    local current_line = vim.fn.line(".", vim.fn.bufwinid(buffer_name))
     local last_line = vim.fn.line("$")
     -- This function is 0-indexed
     local buffer_lines = vim.api.nvim_buf_get_lines(buffer, 0, last_line, {})
     local error_prefix = "Invalid line: "
-    local line
     -- Both loops include current line
     -- Find nearest chunk start
     local nearest_start = get_table_match_index(
@@ -46,6 +62,7 @@ run_chunk = function(buffer)
     end
     -- Exclude start and end lines, which lack valid Python code
     vim.fn["slime#send_range"](nearest_start + 1, nearest_end - 1)
+    return nearest_start + 1, nearest_end - 1
     --local n_lines = nearest_end - nearest_start + 1
     --local lines_to_send = {}
     --
@@ -55,7 +72,6 @@ run_chunk = function(buffer)
     --send_to_repl(table.concat(lines_to_send, "\n"))
 end
 
-send_to_repl = function(text) end
 
 get_table_match_index =
     function(lines, start, stop, step, match_pattern, abort_pattern)
@@ -73,7 +89,7 @@ get_table_match_index =
     end
 
 -- Extract code in chunks from buffer, then join into string
-function parse_code_chunks(buffer)
+function parse_code_chunks(buffer, stop_line)
     -- Get lines from buffer
     if buffer == nil then
         buffer = 0
@@ -84,10 +100,14 @@ function parse_code_chunks(buffer)
         vim.fn.line("$"),
         {}
     )
+    if stop_line  == nil then stop_line = table.getn(buffer_lines) end
+    print(stop_line)
     local code = {}
     local chunk_started, chunk_ended
     local in_chunk = false
-    for _, line in ipairs(buffer_lines) do
+    -- Iterate over whole table even if stop_line specified in order to run contaiing
+    -- chunk if stop_line part of a chunk
+    for i, line in ipairs(buffer_lines) do
         chunk_started = string.match(line, chunk_start)
         chunk_ended = string.match(line, chunk_end)
         -- Case 1: in chunk in last iteration
@@ -105,6 +125,8 @@ function parse_code_chunks(buffer)
             end
             -- Case 2: code inside chunk
         else
+            -- Break early if stop line exceeded
+            if i >= stop_line then  break end
             if chunk_ended then
                 print("Invalid buffer: chunk end outside chunk")
                 return {}
@@ -119,10 +141,68 @@ function parse_code_chunks(buffer)
     return code
 end
 
--- Wrapper to parse all chunks, then run code
-run_all_chunks = function(buffer)
-    -- Wrapper to parse all chunks, then run code
-    local code = parse_code_chunks(buffer)
-    -- Have to join with newlines
-    vim.fn["slime#send"](table.concat(code, "\n"))
+run_all_chunks = create_send_function(parse_code_chunks, function(text) vim.fn["slime#send"](collapse_text(text)) end)
+
+run_chunks_above = create_send_function(function() parse_code_chunks(nil,
+    vim.fn.line(".", vim.fn.bufwinid(vim.api.nvim_buf_get_name(0)))) end, function(text) vim.fn["slime#send"](collapse_text(text)) end)
+
+
+-- Run current line from a buffer
+run_line = function(buffer)
+    if buffer == nil then
+        buffer = 0
+    end
+    local buffer_name = vim.api.nvim_buf_get_name(buffer)
+    -- bufwinid takes a buffer name, not number
+    local current_line = vim.fn.line(".", vim.fn.bufwinid(buffer_name))
+    local line = vim.api.nvim_buf_get_lines(buffer, current_line - 1, current_line , {})
+    vim.fn["slime#send"](collapse_text(line))
 end
+
+
+-- Returns the most recent visual selection in a buffer, regardless of the
+-- mode used
+parse_visual_selection = function(buffer)
+if buffer == nil then
+    buffer = 0
+end
+    -- Define visual registers "'<", "'>" marking selection bounds
+    -- If either missing, return empty string
+    -- Get visual mode (char, line, block)
+    local mode = vim.fn.visualmode()
+    local visual_start = "'<"
+    local visual_end = "'>"
+    local start_line = vim.fn.line(visual_start)
+    local end_line = vim.fn.line(visual_end)
+    local start_col = vim.fn.col(visual_start)
+    local end_col = vim.fn.col(visual_end)
+    -- Change order if needed so start line is higher in buffer
+    if end_line < start_line then
+        start_line, end_line = end_line, start_line
+    end
+    local selected_lines = vim.api.nvim_buf_get_lines(buffer, start_line - 1, end_line, {})
+    --print(vim.inspect(selected_lines))
+    local n_lines = table.getn(selected_lines)
+    -- If only one line selected, start and end col both occur on it, hence
+    -- special case
+    if n_lines == 1 then
+        selected_lines[1] = string.sub(selected_lines[1], start_col, end_col)
+    else
+    -- Trim non-selected parts of start and end lines
+    selected_lines[1] = string.sub(selected_lines[1], start_col)
+    selected_lines[n_lines] = string.sub(selected_lines[n_lines], 1, end_col)
+    end
+    return selected_lines
+    --if mode == "v" then
+    --elseif mode == "V" then
+    --elseif mode == "^V" then
+    --else
+        --print("Invalid visual mode " .. mode)
+        --return ""
+    --end
+end
+send_visual_selection = create_send_function(parse_visual_selection, function(text) vim.fn["slime#send"](collapse_text(text)) end)
+
+--TODO:
+-- 1. Jump to chunks by name/position (absolute or relative)
+-- 2.  Run chunks above
